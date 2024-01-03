ac536500697324e9a31408f0a7139b5391a18156
dromari <7709722@gmail.com>
2024-01-03 20:05:16 +0300

    feat: part og little-bit

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -291,8 +291,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let num2 = num;
+  while (num2 > 0) {
+    if (num2 % 10 === digit) {
+      return true;
+    }
+    num2 = Math.floor(num2 / 10);
+  }
+  return false;
 }
 
 /**
